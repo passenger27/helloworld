9d709385853f2261fedc284885f2954113399d26
user01 <user01@example.com>
2017-01-20 11:16:14 +0800

    created

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.iecas.cisamples.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+	@ Test
+	public void testSayHello()
+	{
+		HelloWorld helloWorld = new HelloWorld();
+		String result = helloWorld.sayHello();
+		assertEquals("Hello Maven", result);
+	}
+}
+
